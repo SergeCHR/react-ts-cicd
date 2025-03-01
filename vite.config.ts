a79dbfe0815580366e28ea8d2a20e81a62b1fc1a
scherneha <serhii.cherneha@prettysmart.it>
2025-03-01 18:03:22 +0200

    removing browser configuration from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,4 @@ import react from "@vitejs/plugin-react"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  test: {
-    browser: {
-      enabled: true,
-      provider: "playwright",
-      instances: [{ browser: "chromium" }],
-    },
-  },
 })
